21561cf345c19c9496c9cc69ab6f20d01fd84017
Rohan Sharma <rsharma@MacBook-Air.local>
2022-08-31 12:45:55 +0530

    update formhash

--- client/src/config.ts
+++ client/src/config.ts
@@ -12,7 +12,7 @@ const config = {
     webRtcUrl: process.env.NODE_ENV === 'development' ? 'ws://localhost:7880' : window.WEBRTC_HOST,
     ssoUrl: 'https://uat-sso.isha.in',
     ssoCallBackUrl: 'https://ieco-api.isha.dev',
-    formHash: '40b69d2784f5c8748ce6f662da34c57f82e6b841f795cb74642e64e160fd96b0',
+    formHash: '212c0c179cee895f7cfb9ccbbf136c365f8fae0fe2847fee8d52ea11548d5860',
     apiKey: '31d9c883155816d15f6f3a74dd79961b0577670ac',
 };
 
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.set('views', path.join(__dirname, buildFolder));
 app.engine('html', require('ejs').renderFile);
 app.use(
-  '/static',
+  '/livekit/static',
   express.static(path.join(__dirname, `${buildFolder}/static`))
 );
 
